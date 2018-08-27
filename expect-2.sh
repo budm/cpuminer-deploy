7af00730be254d77c254f0d9cb84baf4920e3538
budm <budmarv2@gmail.com>
2018-08-27 16:43:29 -0400

    not for us

--- expect-2.sh
+++
@@ -1,6 +0,0 @@
-spawn minerd -a cryptonight -o stratum+tcp://qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x
-expect -re "#"
-send -- "0\r"
-set timeout 60
-expect eof
-
